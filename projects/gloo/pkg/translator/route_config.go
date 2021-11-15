d0e708b3d29ffda109027321288260c3b6158212
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-11-15 11:52:50 -0500

    graphql dummy cluster

--- projects/gloo/pkg/translator/route_config.go
+++ projects/gloo/pkg/translator/route_config.go
@@ -290,11 +290,10 @@ func (t *translatorInstance) setAction(
 		}
 
 	case *v1.Route_GraphqlSchemaRef:
-		out.Action = &envoy_config_route_v3.Route_DirectResponse{
-			DirectResponse: &envoy_config_route_v3.DirectResponseAction{
-				Status: 200, // unused
-				Body: &envoy_config_core_v3.DataSource{
-					Specifier: &envoy_config_core_v3.DataSource_InlineString{InlineString: "unused, graphql overtakes router filter"},
+		out.Action = &envoy_config_route_v3.Route_Route{
+			Route: &envoy_config_route_v3.RouteAction{
+				ClusterSpecifier: &envoy_config_route_v3.RouteAction_Cluster{
+					Cluster: "graphql.dummy.cluster",
 				},
 			},
 		}
